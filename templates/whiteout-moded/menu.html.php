474cded778c22cdd6bd3c59393b18c3958c22bb5
Mephissto <mephissto@gmail.com>
2012-02-18 19:01:31 +0100

    Better indent

--- templates/whiteout-moded/menu.html.php
+++ templates/whiteout-moded/menu.html.php
@@ -1,18 +1,28 @@
 <div id="title">
 	<em>
 		<a href="<?php echo Asaph_Config::$feed_url; ?>" target="_blank">flux rss</a>&nbsp;/&nbsp;
-		<?php if (Asaph_Config::$twitter_account != "") : ?>
-			<a href="<?php echo Asaph_Config::$twitter_account?>" target="_blank">twitter</a>&nbsp;/&nbsp; 
-		<?php endif; ?>
-		<?php if (count(Asaph_Config::$link1) > 0) : ?>
+		<?php
+		if (!empty(Asaph_Config::$twitter_account)) : 
+		?>
+			<a href="<?php echo Asaph_Config::$twitter_account; ?>" target="_blank">twitter</a>&nbsp;/&nbsp; 
+		<?php 
+		endif;
+		if (count(Asaph_Config::$link1) > 0) : 
+		?>
 			<a href="<?php echo Asaph_Config::$link1['url']; ?>" target="_blank"><?php echo Asaph_Config::$link1['title']; ?></a>&nbsp;/&nbsp;
-		<?php endif;?>
-		<?php if (count(Asaph_Config::$link2) > 0) : ?>
+		<?php
+		endif;
+		if (count(Asaph_Config::$link2) > 0) : 
+		?>
 			<a href="<?php echo Asaph_Config::$link2['url']; ?>" target="_blank"><?php echo Asaph_Config::$link2['title']; ?></a>&nbsp;/&nbsp;
-		<?php endif;?>
-		<?php if (count(Asaph_Config::$link3) > 0) : ?>
+		<?php 
+		endif;
+		if (count(Asaph_Config::$link3) > 0) : 
+		?>
 			<a href="<?php echo Asaph_Config::$link3['url']; ?>" target="_blank"><?php echo Asaph_Config::$link3['title']; ?></a>&nbsp;/&nbsp;
-		<?php endif;?>
+		<?php 
+		endif;
+		?>
 		<a href="<?php echo ASAPH_LINK_PREFIX; ?>about">&agrave; propos</a>
 	</em>
 	<h1><a href="<?php echo Asaph_Config::$absolutePath; ?>"><?php echo htmlspecialchars( Asaph_Config::$title ); ?></a></h1>
