4f33b02220bbd61b2c5c014a82a58c5f898d6e84
Mephissto <mephissto@gmail.com>
2012-02-18 18:33:10 +0100

    Check if twitter link is set, if not it will not be displayed

--- templates/whiteout-moded/menu.html.php
+++ templates/whiteout-moded/menu.html.php
@@ -1,7 +1,9 @@
 <div id="title">
 	<em>
-		<a href="<?php echo Asaph_Config::$feed_url; ?>" target="_blank">flux rss</a>&nbsp;/&nbsp; 
-		<a href="<?php echo Asaph_Config::$twitter_account?>" target="_blank">twitter</a>&nbsp;/&nbsp;
+		<a href="<?php echo Asaph_Config::$feed_url; ?>" target="_blank">flux rss</a>&nbsp;/&nbsp;
+		<?php if (Asaph_Config::$twitter_account != "") : ?>
+			<a href="<?php echo Asaph_Config::$twitter_account?>" target="_blank">twitter</a>&nbsp;/&nbsp; 
+		<?php endif; ?>
 		<?php if (count(Asaph_Config::$link1) > 0) : ?>
 			<a href="<?php echo Asaph_Config::$link1['url']; ?>" target="_blank"><?php echo Asaph_Config::$link1['title']; ?></a>&nbsp;/&nbsp;
 		<?php endif;?>
